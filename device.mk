846f1d11c9a142262bc4b1f302ae3e89dd64c6f2
Ethan Chen <intervigil@gmail.com>
2018-11-30 23:28:49 -0800

    chime: Add AOSP RCS packages
    Change-Id: Ic16d3b92820d246db73ad70ccaa0f4bc7f50e515
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>

--- device.mk
+++ device.mk
@@ -492,6 +492,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
 
+# RCS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService
+
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
     RemovePackages
