8f226e5f689b8bf9755086256423a79659a7caaa
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-14 01:48:37 +0200

    chime: Move to common IFAAService Change-Id: I71bfdacc4626d75d56aa12ec384f0f116d6d6ff2
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- chime.mk
+++ chime.mk
@@ -217,11 +217,8 @@ PRODUCT_PACKAGES += \
 
 # IFAA manager
 PRODUCT_PACKAGES += \
-    org.ifaa.android.manager
-
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
-
+    IFAAService
+    
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
