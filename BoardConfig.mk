6b50761b330c59dfd2b0de64cb367c7a90dbae84
xSylla <syllamodder@gmail.com>
2022-07-11 01:15:37 +0200

    chime: Enforce fstab suffix to qcom
    Change-Id: I5b7246196ec57020cdf980a4867b6b9b08ab3087
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,6 +93,7 @@ BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048
 BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.fstab_suffix=qcom
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_CMDLINE += kpti=off
 
