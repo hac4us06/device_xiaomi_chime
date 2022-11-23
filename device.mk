fc4349575b986c08bf3e15daa5810c195b388545
hac4us06 <hac4usanime@gmail.com>
2022-11-23 20:25:54 +0000

    Revert "chime: Bring back Xiaomi Parts"
    This reverts commit 260816c6591f34d8ae9ea45fff67cf34a9587418.

--- device.mk
+++ device.mk
@@ -430,10 +430,6 @@ PRODUCT_PACKAGES += \
     SystemUIOverlayChime \
     TelephonyOverlayChime
 
-# Parts
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # Perf
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2 \
