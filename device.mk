748dbd827bcad1885acab16cf29cad71f429cc19
EndCredits <alicization.han@gmail.com>
2022-12-27 10:16:11 +0800

    chime: Also set speed profile for Settings app
    Signed-off-by: EndCredits <alicization.han@gmail.com>
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

--- device.mk
+++ device.mk
@@ -399,6 +399,7 @@ PRODUCT_PACKAGES += \
 # Preopt SystemUI.
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUIGoogle  # For internal
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI  # For AOSP
+PRODUCT_DEXPREOPT_SPEED_APPS += Settings
 
 # Compile SystemUI on device with `speed`.
 PRODUCT_PROPERTY_OVERRIDES += \
