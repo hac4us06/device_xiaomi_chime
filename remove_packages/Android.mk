fffecf7e0d179541827b04c0b0da3670f7a157ee
hac4us06 <hac4usanime@gmail.com>
2023-02-06 23:06:10 +0000

    chime: Remove unwanted bloatware
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

---
+++ remove_packages/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    Chrome \
+    Drive \
+    GoogleCamera \
+    Maps \
+    Music \
+    YouTube \
+    YouTubeMusicPrebuilt
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
