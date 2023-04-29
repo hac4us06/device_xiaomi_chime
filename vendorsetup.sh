46e1f33c8b64ef5984f0507c21dede035ffc683f
hac4us06 <hac4usanime@gmail.com>
2023-04-29 11:08:48 +0000

    chime: track lineage hardware repo
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,6 +9,7 @@ sleep 1
 
 # Clone kernel
 git clone https://github.com/hac4us06/kernel_xiaomi_chime -b pyro-wip kernel/xiaomi/chime
+
 # Setup kernel
 (cd kernel/xiaomi/chime; git submodule init; git submodule update; sed -i 's/#ifdef CONFIG_KPROBES/#if 0/g' KernelSU/kernel/ksu.c)
 
@@ -16,5 +17,7 @@ git clone https://github.com/hac4us06/kernel_xiaomi_chime -b pyro-wip kernel/xia
 git clone https://gitlab.com/hac4us06/vendor-xiaomi-chime-13 -b lineage-20 vendor/xiaomi/chime
 git clone https://gitlab.com/hac4us06/vendor-xiaomi-citrus-13 -b lineage-20 vendor/xiaomi/citrus
 git clone https://gitlab.com/hac4us06/vendor-xiaomi-lime-13 -b lineage-20 vendor/xiaomi/lime
+
 # Hardware
-git clone https://github.com/hac4us06/hardware_xiaomi -b thirteen hardware/xiaomi
+rm -rf hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
