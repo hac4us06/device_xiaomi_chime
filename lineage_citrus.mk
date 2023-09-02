f1bfe835e3a0b6ce73a0fe3758636cfe6492f015
hac4us06 <hac4usanime@gmail.com>
2023-09-02 15:19:46 +0200

    chime: Bringup AlphaDroid
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

--- lineage_citrus.mk
+++ lineage_citrus.mk
@@ -12,27 +12,31 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Alpha prop
 
-#ALPHA_BUILD_TYPE := Official
+ALPHA_BUILD_TYPE := Official
+ALPHA_MAINTAINER := IczYn x Hac4us
 
- 
-
-ALPHA_MAINTAINER := IczYn
-
- 
-
-# Alpha device prop
-
+# Device config
+TARGET_HAS_UDFPS := false
 TARGET_ENABLE_BLUR := true
-
-TARGET_INCLUDE_MATLOG := false
-
-#TARGET_USE_GRAPHENE_CAMERA := true
-
-TARGET_USE_PIXEL_LAUNCHER := false
-
 TARGET_EXCLUDES_AUDIOFX := true
-
 TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# GAPPS
+WITH_GAPPS := false
+TARGET_INCLUDE_GOOGLE_APP := false
+TARGET_INCLUDE_GOOGLE_CALCULATOR := false
+TARGET_INCLUDE_GOOGLE_CALENDAR := false
+TARGET_INCLUDE_GOOGLE_CHROME := false
+TARGET_INCLUDE_GOOGLE_DESKCLOCK := false
+TARGET_INCLUDE_GOOGLE_DRIVE := false
+TARGET_INCLUDE_GMAIL := false
+TARGET_INCLUDE_GOOGLE_MAPS := false
+TARGET_INCLUDE_GOOGLE_PHOTOS := false
+TARGET_INCLUDE_GOOGLE_SETUP := false
+
+# Debugging
+TARGET_INCLUDE_MATLOG := false
+TARGET_DEFAULT_ADB_ENABLED := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_citrus
