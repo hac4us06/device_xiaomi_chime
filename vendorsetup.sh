af1a7e4452c4d842f0951d830a6a1e7212337ccd
hac4us06 <hac4usanime@gmail.com>
2023-09-25 10:41:38 +0000

    chime: sync vendor tree
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,14 +1,17 @@
 # ROM source patches
+
 color="\033[0;32m"
 end="\033[0m"
 echo -e "${color}Applying patches${end}"
 sleep 1
+
 # Clone kernel
 git clone https://github.com/hac4us06/kernel-xiaomi-electro -b main kernel/xiaomi/chime
 # Setup kernel
 (cd kernel/xiaomi/chime; git submodule init; git submodule update; sed -i 's/#ifdef CONFIG_KPROBES/#if 0/g' KernelSU/kernel/ksu.c)
+
 # Clone vendor
-git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_chime -b thirteen vendor/xiaomi/chime
+git clone https://gitlab.com/hac4us06/vendor_xiaomi_chime -b thirteen vendor/xiaomi/chime
 git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_citrus.git -b thirteen vendor/xiaomi/citrus
 git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_lime.git -b thirteen vendor/xiaomi/lime
 
