2e16399e7e4fd927da355ab93c7b8166434d7994
hac4us06 <hac4usanime@gmail.com>
2023-10-05 07:03:59 +0000

    Revert "chime: Alpha bringup"
    This reverts commit cf8585e3f5cb1b3cc6a777cc5269030d937eb199.

--- Android.mk
+++ Android.mk
@@ -132,14 +132,6 @@ $(CNE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@rm -rf $@
 	$(hide) ln -sf /vendor/lib64/$(notdir $@) $@
 
-WFDSERVICE_LIBRARIES := libwfdnative.so
-
-WFDSERVICE_SYMLINKS := $(addprefix $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/WfdService/lib/arm64/,$(notdir $(WFDSERVICE_LIBRARIES)))
-$(WFDSERVICE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /system/system_ext/lib64/$(notdir $@) $@
-
 ALL_DEFAULT_INSTALLED_MODULES += $(CNE_SYMLINKS)
 
 endif
