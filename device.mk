0b56a327bb6d9f03dbf93e66d2c456e55fb51d20
basamaryan <basam.aryan@gmail.com>
2023-10-07 15:17:04 -0400

    chime: Build protobuf vendorcompat
    Change-Id: I724e933e046cbf631c078321d55550b96ca998da
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

--- device.mk
+++ device.mk
@@ -394,6 +394,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
