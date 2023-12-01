943b9b825ca725e384e3a16e6dd858c7b6520d0f
hac4us06 <hac4usanime@gmail.com>
2023-12-01 16:56:35 +0100

    chime: derp bringup
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,13 +5,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_citrus.mk \
-    $(LOCAL_DIR)/aosp_lime.mk
+    $(LOCAL_DIR)/derp_citrus.mk \
+    $(LOCAL_DIR)/derp_lime.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_citrus-user \
-    aosp_citrus-userdebug \
-    aosp_citrus-eng \
-    aosp_lime-user \
-    aosp_lime-userdebug \
-    aosp_lime-eng
+    derp_citrus-user \
+    derp_citrus-userdebug \
+    derp_citrus-eng \
+    derp_lime-user \
+    derp_lime-userdebug \
+    derp_lime-eng
--- custom.mk
+++ custom.mk
@@ -1,23 +1,14 @@
 #
-# Copyright (C) 2022 The PixelExperience Project
 #           (C) 2023 Lilium Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Derpfest stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Live Wallpaper
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
-
-# Google Assistant (New Generation)
-PRODUCT_PACKAGES += \
-    NgaResources \
-    nga
-
-# Recovery
-TARGET_USES_AOSP_RECOVERY := true
+# Officials Flag
+DERP_BUILDTYPE := Official
