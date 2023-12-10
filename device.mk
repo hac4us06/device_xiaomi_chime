745cf40ca3af24ea95892f9f3b829c592bd6014a
Avinash Dwivedi <141247046+AvinashD1010@users.noreply.github.com>
2023-12-10 12:14:35 +0530

    chime: Fix overlays
    - Improved WiFi and brightness from moto
    
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

--- device.mk
+++ device.mk
@@ -324,7 +324,6 @@ PRODUCT_PACKAGES += \
 # Overlays
 PRODUCT_PACKAGES += \
     CarrierConfigOverlay \
-    FrameworksCustomChime \
     FrameworksResChime \
     SettingsResChime \
     SystemUIResChime \
