f59faac3f71cd60a73a2b2154e1e8a3cb7ad70f6
HeroBuxx <me@herobuxx.me>
2024-02-10 07:03:40 +0000

    chime: Enchanting an Ursinia
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- ChimeConfig.mk
+++ ChimeConfig.mk
@@ -93,15 +93,11 @@ TARGET_HALS_PATH ?= hardware/qcom-caf/$(TARGET_BOARD_PLATFORM)
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/vintf/framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    $(COMMON_PATH)/vintf/framework_compatibility_matrix.xml
 
 
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/vintf/compatibility_matrix.xml
-
-# Health - LineageOS
-include device/xiaomi/chime/LineageConfig.mk
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):init_chime
---
+++ halcyon.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2023 Lilium Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common Halcyon stuff.
+$(call inherit-product, vendor/halcyon/config/common.mk)
