f4f3832a305e368099ee9bc4c261007a4db778d9
Soo-Hwan Na <whiteshell2544@naver.com>
2024-02-15 20:50:47 +0900

    chime: vendorsetup: Use --depth=1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,14 +4,14 @@ end="\033[0m"
 echo -e "${color}Applying patches${end}"
 sleep 1
 # Clone kernel
-git clone https://github.com/hac4us06/kernel-xiaomi-electro -b main kernel/xiaomi/chime
+git clone https://github.com/hac4us06/kernel-xiaomi-electro -b main kernel/xiaomi/chime --depth=1
 # Setup kernel
 #(cd kernel/xiaomi/chime; git submodule init; git submodule update; sed -i 's/#ifdef CONFIG_KPROBES/#if 0/g' KernelSU/kernel/ksu.c)
 #(cd kernel/xiaomi/chime; cd KernelSU; git pull origin main; git reset --hard; cd ..; git add KernelSU)
 # Clone vendor
-git clone https://github.com/hac4us06/vendor_xiaomi_chime -b 13 vendor/xiaomi/chime
-git clone https://github.com/hac4us06/vendor_xiaomi_citrus -b 13 vendor/xiaomi/citrus
-git clone https://github.com/hac4us06/vendor_xiaomi_lime -b 13 vendor/xiaomi/lime
+git clone https://github.com/hac4us06/vendor_xiaomi_chime -b 13 vendor/xiaomi/chime  --depth=1
+git clone https://github.com/hac4us06/vendor_xiaomi_citrus -b 13 vendor/xiaomi/citrus  --depth=1
+git clone https://github.com/hac4us06/vendor_xiaomi_lime -b 13 vendor/xiaomi/lime  --depth=1
 
 # Hardware
-git clone https://github.com/hac4us06/hardware_xiaomi -b thirteen hardware/xiaomi
+git clone https://github.com/hac4us06/hardware_xiaomi -b thirteen hardware/xiaomi  --depth=1
