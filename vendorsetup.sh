74f96ef3b17b514da00343ae459ed47901bc7353
hac4us06 <hac4usanime@gmail.com>
2024-02-18 09:02:02 +0100

    chime: update kernel branch
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,7 @@ end="\033[0m"
 echo -e "${color}Applying patches${end}"
 sleep 1
 # Clone kernel
-git clone https://github.com/hac4us06/kernel-xiaomi-electro -b main kernel/xiaomi/chime --depth=1
+git clone https://github.com/hac4us06/kernel-xiaomi-electro -b 14 kernel/xiaomi/chime --depth=1
 # Setup kernel
 #(cd kernel/xiaomi/chime; git submodule init; git submodule update; sed -i 's/#ifdef CONFIG_KPROBES/#if 0/g' KernelSU/kernel/ksu.c)
 #(cd kernel/xiaomi/chime; cd KernelSU; git pull origin main; git reset --hard; cd ..; git add KernelSU)
