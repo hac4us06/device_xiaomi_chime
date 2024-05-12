99e340684abfac91b6384d974692cb315ea6a665
HeroBuxx <me@herobuxx.me>
2024-05-12 15:13:55 +0000

    chime: Bring back Lineage Health
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- halcyon.mk
+++ halcyon.mk
@@ -6,3 +6,7 @@
 
 # Inherit some common Halcyon stuff.
 $(call inherit-product, vendor/halcyon/config/common.mk)
+
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
