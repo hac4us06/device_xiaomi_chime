378fc6bb7a30cb5a5c87fd1ff825b6dbc1addaec
HeroBuxx <me@herobuxx.me>
2024-06-07 17:46:45 +0000

    chime: Bring back NFC HIDL
    This reverts commit cdc1e9b4bbe95e23b20b1bed29d86b4b175c5682.
    
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- lime/BoardConfig.mk
+++ lime/BoardConfig.mk
@@ -12,5 +12,9 @@ include device/xiaomi/chime/ChimeConfig.mk
 # Assert
 TARGET_OTA_ASSERT_DEVICE := lime,lemon,pomelo,chime,juice
 
+# NFC
+ODM_MANIFEST_SKUS += lemon
+ODM_MANIFEST_LEMON_FILES := $(DEVICE_PATH)/vintf/manifest_lemon.xml
+
 # Inherit from the proprietary version
 include vendor/xiaomi/lime/BoardConfigVendor.mk
