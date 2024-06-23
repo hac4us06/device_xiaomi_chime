5917e2d5863f9b7a3b3d60bd96ebbb0f397d772e
Bruno Martins <bgcngm@gmail.com>
2024-06-23 21:58:49 +0100

    chime: Move to new RFS install_symlink targets Change-Id: I9560fd0da00958d9349a0881b5a5f64a2c02e495
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- chime.mk
+++ chime.mk
@@ -20,6 +20,9 @@ $(call inherit-product, vendor/xiaomi/chime/chime-vendor.mk)
 # Inherit some common Halcyon stuff
 $(call inherit-product, device/xiaomi/chime/halcyon.mk)
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 # Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
