a6b0b000af27d10fd7615bc7d29d31b8a01a4177
hac4us06 <hac4usanime@gmail.com>
2024-07-06 06:13:53 +0000

    chime: add vendorsetup.sh
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,16 @@
+# ROM source patches
+color="\033[0;32m"
+end="\033[0m"
+echo -e "${color}Applying patches${end}"
+sleep 1
+
+# Kernel
+git clone https://github.com/liliumproject/kernel_xiaomi_chime kernel/xiaomi/chime --depth=1
+
+# Vendor
+git clone https://www.opencode.net/liliumproject/vendor_xiaomi_chime vendor/xiaomi/chime --depth=1
+git clone https://www.opencode.net/liliumproject/vendor_xiaomi_citrus vendor/xiaomi/citrus
+git clone https://www.opencode.net/liliumproject/vendor_xiaomi_lime vendor/xiaomi/lime
+
+# Hardware
+git clone https://github.com/hac4us06/android_hardware_xiaomi hardware/xiaomi 
