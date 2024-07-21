a88436c8f4b62c17e927609a9540393ee877d1a3
HeroBuxx <me@herobuxx.me>
2024-07-21 07:47:46 +0000

    chime: Drop dead targets and import missing FCM entries
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- ChimeConfig.mk
+++ ChimeConfig.mk
@@ -100,6 +100,7 @@ TARGET_HALS_PATH ?= hardware/qcom-caf/$(TARGET_BOARD_PLATFORM)
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/vintf/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/vintf/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 
