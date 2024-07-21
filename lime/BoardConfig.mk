42f0401188cbc0074bbe4ea8dd864607996e1ab5
hac4us06 <hac4usanime@gmail.com>
2024-07-21 19:11:59 +0000

    Revert "chime: Drop prebuilt DTB/O images"
    This reverts commit 426b59b9474580192022f64bd995c594d4681756.
    
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

--- lime/BoardConfig.mk
+++ lime/BoardConfig.mk
@@ -16,5 +16,11 @@ TARGET_OTA_ASSERT_DEVICE := lime,lemon,pomelo,chime,juice
 ODM_MANIFEST_SKUS += lemon
 ODM_MANIFEST_LEMON_FILES := $(DEVICE_PATH)/vintf/manifest_lemon.xml
 
+# Kernel - Prebuilt images
+#BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/images/dtbo.img
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/images/dtb.img
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+
 # Inherit from the proprietary version
 include vendor/xiaomi/lime/BoardConfigVendor.mk
