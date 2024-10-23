b571545942d18e62e32874f336aa6e6901a64da1
royna2544 <whiteshell2544@naver.com>
2024-10-23 10:09:46 +0900

    chime: Fix soong namespace inclusion

--- chime.mk
+++ chime.mk
@@ -35,7 +35,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/halcyon/interfaces/power-libperfmgr \
+    hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi
 
