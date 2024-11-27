876bd6d9a16e315dde2d66a2de3d971d498a3964
ash(hac4us) <116029625+hac4us06@users.noreply.github.com>
2024-11-27 22:54:53 +0530

    chime: Switch to AIDL USB HAL

--- chime.mk
+++ chime.mk
@@ -528,7 +528,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb-service.qti
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
