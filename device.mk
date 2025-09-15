0821007774b2ac241f1d1c4a9e2b7105df850c51
Yumi Yukimura <me.cafebabe@gmail.com>
2025-09-15 23:16:34 +0700

    chime: Migrate to QTI USB Gadget AIDL HAL

--- device.mk
+++ device.mk
@@ -548,7 +548,9 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.qti
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti \
+    usb_compositions.conf
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
