13504fd298b5cc176cabe86db20a0b4b08ac4eb5
hac4us06 <hac4usanime@gmail.com>
2025-10-21 15:00:15 +0000

    chime: derp bringup
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

--- device.mk
+++ device.mk
@@ -6,7 +6,7 @@
 
 # Don't forget to sign your builds:
 # https://gist.github.com/A2L5E0X1/54cb1b3a49030a9ebf8608b4e68073f5
-include vendor/lineage-priv/keys/keys.mk
+#include vendor/lineage-priv/keys/keys.mk
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
