442068167eaef88e2bdf1c34766b1b980d3e9cf0
hac4us06 <hac4usanime@gmail.com>
2025-10-21 17:59:11 +0530

    Revert "chime: Migrate to QTI USB Gadget AIDL HAL"
    This reverts commit 0821007774b2ac241f1d1c4a9e2b7105df850c51.
    
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

--- device.mk
+++ device.mk
@@ -548,9 +548,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.qti \
-    android.hardware.usb.gadget-service.qti \
-    usb_compositions.conf
+    android.hardware.usb-service.qti
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
