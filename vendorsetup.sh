0088453382cb1338c34f80324227e5d19ad45094
hac4us06 <hac4usanime@gmail.com>
2025-10-21 16:56:19 +0530

    chime: Update repos
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,11 +5,10 @@ echo -e "${color}Cloning Tree${end}"
 sleep 1
 
 # Kernel
-git clone https://github.com/kidzz1965/valeryn_xiaomi_sm6115.git kernel/xiaomi/chime 
+git clone https://github.com/hac4us06/kernel_xiaomi_chime -b unified kernel/xiaomi/chime
 
 # Vendor
-git clone https://github.com/joe7500/vendor_xiaomi_chime -b lineage-23.0 vendor/xiaomi/chime 
-
+git clone https://github.com/hac4us06/vendor_xiaomi_chime -b unified vendor/xiaomi/chime
 
 # Hardware
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi 
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi
