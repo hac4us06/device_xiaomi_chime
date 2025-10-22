d2c7b9e78aa7708438e3f16145ba338825ece142
hac4us06 <hac4usanime@gmail.com>
2025-10-22 18:27:23 +0000

    chime: unset Reserve space for gapps
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

--- lineage_chime.mk
+++ lineage_chime.mk
@@ -33,6 +33,3 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 
 TARGET_ENABLE_BLUR := false
 
-# Set to true for building a ROM without GAPPS
-# Set to false for building a ROM with GAPPS included
-RESERVE_SPACE_FOR_GAPPS := true
